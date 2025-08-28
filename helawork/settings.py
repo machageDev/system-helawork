75ae32d73b5e27431ec2ab50190d89b5b0a518cd
machageDev <machagefranklyn@gmail.com>
2025-08-28 14:31:47 +0300

    connected postgresql

--- helawork/settings.py
+++ helawork/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'helawork.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'helaworkdb',       # your database name
+        'USER': 'postgres',         # your postgres username
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
