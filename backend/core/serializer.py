a35243ec12eef86a6a9c196a661fe8ad3cf23028
machageDev <machagefranklyn@gmail.com>
2025-08-29 00:10:04 +0300

    empliyer serializer

--- backend/core/serializer.py
+++ backend/core/serializer.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from models import User
+from .models import Employer
 
 class UserSerializer(serializers.ModelField):
     class meta:
         model = User
         fields = "__all__"
+
+
+
+class EmployerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employer
+        fields = '__all__'
         
