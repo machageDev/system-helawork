17e34ea476d669f060d6f06ee032358ff277316a
machageDev <machagefranklyn@gmail.com>
2025-08-29 14:13:19 +0300

    profile api added

--- backend/core/views.py
+++ backend/core/views.py
@@ -133,4 +133,11 @@ def apiforgot_password(request):
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)        
     
-  
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def apicreate_user_profile(request):
+    serializer = UserProfileSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
