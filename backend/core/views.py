231b25a15f992a91d8207cdbd47ef6207bfec9be
machageDev <machagefranklyn@gmail.com>
2025-08-29 09:29:30 +0300

    added python function that takes https request and return https requst

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User
+from rest_framework import serializers, viewsets, permissions, status
+
+
