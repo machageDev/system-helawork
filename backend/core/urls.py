fa5f00524399325a7a3e1904160644e775b07d72
machageDev <machagefranklyn@gmail.com>
2025-08-30 20:45:26 +0300

    api proof of wrk

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('apiforgot_password',views.apiforgot_password, mame = 'apiforgotpassword'),
     path('apicreate_profile',views.apicreate_user_profile,name='apicreaateprofile'),
     path('worklogs/', views.worklog_list, name='worklog-list'),
-    path('worklogs/<int:pk>/', views.worklog_detail, name='worklog-detail')
+    path('worklogs/<int:pk>/', views.worklog_detail, name='worklog-detail'),
+    path('proofs', views.proof_list, name='proof-list'),
+    path('proofs', views.proof_detail, name='proof-detail'),
     
 ]
