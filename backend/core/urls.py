20c2a9f5988e443b9bae363bf8683aeb2bc982a9
machageDev <machagefranklyn@gmail.com>
2025-08-30 20:35:29 +0300

    urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('apilogin ',views.apilogin, name = 'apilogin'),
     path('apiregister',views.apiregister,name='apiregister'),
     path('apiforgot_password',views.apiforgot_password, mame = 'apiforgotpassword'),
-    path('apicreate_profile',views.apicreate_user_profile,name='apicreaateprofile')
+    path('apicreate_profile',views.apicreate_user_profile,name='apicreaateprofile'),
+    path('worklogs/', views.worklog_list, name='worklog-list'),
+    path('worklogs/<int:pk>/', views.worklog_detail, name='worklog-detail')
     
 ]
