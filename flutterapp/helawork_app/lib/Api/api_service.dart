a745d3b31d36caaff69452d438d4095558ae96fd
machageDev <machagefranklyn@gmail.com>
2025-08-31 23:06:09 +0300

    create apiservice.dart for register api

---
+++ flutterapp/helawork_app/lib/Api/api_service.dart
@@ -0,0 +1,35 @@
+
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService{
+  static const String baseUrl = 'http://192.168.188.100:8000';
+  static const String registerUrl = '$baseUrl/apiregister';
+
+
+  static Future<Map<String, dynamic>>register(
+    String name,String email, String password,String phoneNO ) async{
+      final url = Uri.parse(registerUrl);
+      try{
+        final response = await http.post(
+          url,
+          headers:{"content-Type ":"application/json"},
+          body:jsonEncode({
+            "name":name,
+            "email":email,
+            "password":password,
+            "phoneNO":phoneNO,
+          }),
+        );
+        if (response.statusCode == 201){
+          return {"success":true};
+            
+        }else{
+          return{"success":false,"message":jsonDecode(response.body)["error"]};
+        }
+      }catch(e){
+        return{"success":false,"message":"Network error,please try again."};
+      }
+    }
+}
