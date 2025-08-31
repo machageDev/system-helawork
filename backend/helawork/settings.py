2cfd269f0db7e4e0c5630845f0a3008995f31c4b
machageDev <machagefranklyn@gmail.com>
2025-08-31 12:49:53 +0300

    allowed hosts

--- backend/helawork/settings.py
+++ backend/helawork/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wnli4_56vo0r%3n)6m_qeu5k^@2@(dyry=rm+cp1ic^fw939pq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
